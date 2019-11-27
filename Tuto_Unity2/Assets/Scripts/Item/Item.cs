0aa0b52a43e6ad82a717c778b48dc9191122f18d
d0lm3nh1r <d0lm3nh1r@gmail.com>
2019-11-27 13:56:00 +0100

    Add inventory code

--- Tuto_Unity2/Assets/Scripts/Item/Item.cs
+++ Tuto_Unity2/Assets/Scripts/Item/Item.cs
@@ -6,4 +6,11 @@ public class Item : ScriptableObject
    new public string name = "New Item";
    public Sprite icon = null;
    public bool isDefaultItem = false;
+
+   public virtual void Use(){
+      //use the item
+      //something might happen
+
+      Debug.Log("Using "+ name);
+   }
 }
